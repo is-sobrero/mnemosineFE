1e45486482cd828377b0a43bf2124f9a1848cfb4
Ornitorink0 <matteogurri1@gmail.com>
2024-07-12 15:01:14 +0200

    Aggiunta dell'esercizio 104 e aggiunta di alcuni assets fruits

--- src/app/esercizi/esercizi.routes.ts
+++ src/app/esercizi/esercizi.routes.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Es101Component } from './es101/es101.component';
 import { Es102Component } from './es102/es102.component';
-
+import { Es104Component } from './es104/es104.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/esercizi', pathMatch: 'full'},
+  { path: '', redirectTo: '/esercizi', pathMatch: 'full' },
   {
     path: '101',
     component: Es101Component,
@@ -13,6 +13,10 @@ const routes: Routes = [
   {
     path: '102',
     component: Es102Component,
+  },
+  {
+    path: '104',
+    component: Es104Component,
   },
 ];
 
