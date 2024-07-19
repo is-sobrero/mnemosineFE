691fe9e58de80233a92493c40b46822b1df55a4b
Alex230805 <misuralealessandro@gmail.com>
2024-07-19 18:03:45 +0200

    Esercizio 1 della sezione "linguaggio" implementato

--- src/app/esercizi/esercizi.routes.ts
+++ src/app/esercizi/esercizi.routes.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Es101Component } from './es101/es101.component';
 import { Es102Component } from './es102/es102.component';
-
+import { Es401Component } from './es401/es401.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/esercizi', pathMatch: 'full'},
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: '102',
     component: Es102Component,
   },
+  {
+    path: "401",
+    component: Es401Component,
+  }
 ];
 
 @NgModule({
