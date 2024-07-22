e5922fcaa9993ffd21a824d426df87d21edd52c0
Alex230805 <misuralealessandro@gmail.com>
2024-07-22 17:14:35 +0200

    implementazione esercizio 402, mancante la sezione audio. Vedere "todo.md" per dettagli e problemi riscontrati

--- src/app/esercizi/es402/es402.component.ts
+++ src/app/esercizi/es402/es402.component.ts
@@ -8,6 +8,9 @@ import { MatButton } from '@angular/material/button';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { __await } from 'tslib';
 
+import * as googleTTS from "google-tts-api";
+
+
 @Component({
   selector: 'app-es402',
   standalone: true,
@@ -32,6 +35,7 @@ import { __await } from 'tslib';
 })
 export class Es402Component implements OnInit{
   //private GoogleTTS:string = "https://translate.google.com/translate_tts?ie=UTF-8&tl=it-IT&client=tw-ob&q=";
+  
   private connection = inject(HttpClient);
   private dictionary:string[] = [];
   private selectionCache:string[] = [];
@@ -56,8 +60,7 @@ export class Es402Component implements OnInit{
   errors:number = 0;
   display:string = "";
   word_typed = "";
-  speach:any = null;
-
+  speech:any =  null;
 
  /*
   *  Level: varable used to set the difficulty
@@ -85,25 +88,83 @@ export class Es402Component implements OnInit{
       for(var i=0;i<3;i++){
         this.getWord(this.difficulty.at(i),this.dictionary,this.list_of_words);
       }
-      this.setWord();
+      this.setWord(false);
     });
   }
 
   checkContent(){
     if(this.display == this.word_typed){
       this.current_state = 1;
+      var obj:any = document.querySelector(".word_container_full");
+      obj.style.display = "flex"; 
     }else{
       this.errors += 1;
     }
   }
 
-  setWord():void{
+  setWord(redoing:boolean):void{
     this.display = ""+this.list_of_words.at(this.level);
-    /*
-      Use google text to speach api 
-    */
-    console.log(this.speach);
+    var container:any = document.querySelector(".word_container");
+    var container_full:any = document.querySelector(".word_container_full");
+   
+    var text:any = null;
+    var text_2:any = null;
+    
+    var obj:any = null;
+    var obj_2:any = null;
+    
+    var cell_style:string = "padding:15px!important;margin:5px!important;margin-left:10px!important;margin-right:10px!important;border: 3px solid #383838!important;border-radius: 5px!important;width: min-content!important;height: min-content!important;filter: drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06));";
+    var obj_style:string = "display: flex!important;flex-direction: row!important;justify-content: center!important;align-content: center!important;margin-top:40px!important;margin-bottom:30px!important;";
+    
+    var random_character:any = null;
+        
+    random_character = Math.floor(Math.random()*this.display.length);
+
+  if(redoing === true){
+    container.innerHTML = "";
+    container_full.innerHTML = "";
   }
+
+    /* this.speech = this.GoogleTTS+this.display; */
+
+    /* this.speech = googleTTS.getAudioUrl(this.display, {
+       lang: "it",
+        slow: false,
+        host: "https://translate.google.com"
+      });
+    */
+
+    
+    obj = document.createElement("div");
+    obj_2 = document.createElement("div");
+    obj.setAttribute("class", "cell_container");
+    obj.setAttribute("style", obj_style);
+    obj_2.setAttribute("class", "cell_container");
+    obj_2.setAttribute("style", obj_style);
+
+    for(var i=0;i<this.display.length; i++){
+      text = document.createElement("h1");
+      text_2 = document.createElement("h1");
+      text.setAttribute("class", "cell")
+      text.setAttribute("style", cell_style);
+      text_2.setAttribute("class", "cell")
+      text_2.setAttribute("style", cell_style);
+
+      text_2.innerHTML += this.display.at(i);
+      if(i!=random_character){
+        text.innerHTML = this.display.at(i);
+      }else{
+        text.innerHTML = "-";
+      }
+      obj.appendChild(text);
+      obj_2.appendChild(text_2);
+    }
+    container.appendChild(obj);
+    container_full.appendChild(obj_2);
+
+    console.log(this.display);
+  }
+
 
   private getWord(len:any, dictionary:string[], list_of_words:string[]):void{
     var word:string = "";
