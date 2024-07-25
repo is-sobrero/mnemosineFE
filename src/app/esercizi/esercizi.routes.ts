850ffab544ef9215517e1d4a0d5dca9d71d3b2f2
GiacoLup <giacomoluppino8@gmail.com>
2024-07-25 18:38:18 +0200

    [COMPLETATO] Esercizio 303

--- src/app/esercizi/esercizi.routes.ts
+++ src/app/esercizi/esercizi.routes.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Es101Component } from './es101/es101.component';
-import { Es102Component } from './es102/es102.component';
 import { Es303Component } from './es303/es303.component';
 
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/esercizi', pathMatch: 'full'},
-  {
-    path: '101',
-    component: Es101Component,
-  },
-  {
-    path: '102',
-    component: Es102Component,
-  },
   {
     path: '303',
     component: Es303Component,
