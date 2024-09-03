2cd1c82f6fbc2b2e9add450b8ca065385a94d9aa
Ettore Pane <ettore.pane@skywarder.eu>
2024-09-03 10:28:11 +0200

    Api & Login

--- src/app/esercizi/esercizi.routes.ts
+++ src/app/esercizi/esercizi.routes.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Es101Component } from './es101/es101.component';
-import { Es102Component } from './es102/es102.component';
 import { Es103Component } from './es103/es103.component';
 import { Es104Component } from './es104/es104.component';
 import { Es105Component } from './es105/es105.component';
@@ -11,10 +9,6 @@ import { Es110Component } from './es110/es110.component';
 import { Es303Component } from './es303/es303.component';
 const routes: Routes = [
   { path: '', redirectTo: '/esercizi', pathMatch: 'full' },
-  {
-    path: '101',
-    component: Es101Component,
-  },
   {
     path: '110',
     component: Es110Component,
