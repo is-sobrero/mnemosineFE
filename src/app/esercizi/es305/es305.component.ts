00bcb0d4ae1cb221d5ecc7737742015c17acc04e
caoschr1 <caoschr1@privify.me>
2024-09-03 16:32:47 +0200

    component creato

---
+++ src/app/esercizi/es305/es305.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-es305',
+  standalone: true,
+  imports: [],
+  templateUrl: './es305.component.html',
+  styleUrl: './es305.component.scss'
+})
+export class Es305Component {
+
+}
