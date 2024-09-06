d41a5970fc780ce192d7a6bf05880fd65b781bf5
Ornitorink0 <matteogurri1@gmail.com>
2024-09-06 08:08:15 +0200

    Aggiunta esercizio 506 (Non Terminato)

---
+++ src/app/esercizi/es506/es506.component.ts
@@ -0,0 +1,133 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { NgFor, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import {
+  MatCard,
+  MatCardActions,
+  MatCardHeader,
+  MatCardTitle,
+  MatCardSubtitle,
+  MatCardContent,
+} from '@angular/material/card';
+import { __values } from 'tslib';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-es506',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardActions,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+    MatFormField,
+    MatLabel,
+    MatButton,
+    MatInputModule,
+    NgFor,
+    NgIf,
+    HttpClientModule,
+  ],
+  templateUrl: './es506.component.html',
+  styleUrl: './es506.component.scss'
+})
+export class Es506Component implements OnInit {
+  step = 1;
+  sequenzaImmagini: any = [];
+  immagineCorrente: any;
+  sequenzaSelezionata: any = []
+  timer: any;
+  livello = 1;
+  errori = 0;
+
+  elementiLivello1 = [
+    { id: 0, src: '../../assets/es506_images/lvl1/albero.png' },
+    { id: 1, src: '../../assets/es506_images/lvl1/cane.png' },
+    { id: 2, src: '../../assets/es506_images/lvl1/cipolla.png' },
+    { id: 3, src: '../../assets/es506_images/lvl1/delfino.png' },
+    { id: 4, src: '../../assets/es506_images/lvl1/gatto.png' },
+    { id: 5, src: '../../assets/es506_images/lvl1/mela.png' },
+  ]
+
+  elementiLivello2 = [
+    { id: 6, src: '../../assets/es506_images/lvl2/carota.png' },
+    { id: 7, src: '../../assets/es506_images/lvl2/computer.png' },
+    { id: 8, src: '../../assets/es506_images/lvl2/finestra.png' },
+    { id: 9, src: '../../assets/es506_images/lvl2/orologio.png' },
+    { id: 10, src: '../../assets/es506_images/lvl2/penna.png' },
+    { id: 11, src: '../../assets/es506_images/lvl2/quercia.png' },
+  ]
+
+  elementiLivello3 = [
+    { id: 12, src: '../../assets/es506_images/lvl3/avocado.png' },
+    { id: 13, src: '../../assets/es506_images/lvl3/dragon-fruit.png' },
+    { id: 14, src: '../../assets/es506_images/lvl3/mela.png' },
+    { id: 15, src: '../../assets/es506_images/lvl3/melanzana.png' },
+    { id: 16, src: '../../assets/es506_images/lvl3/melone.png' },
+    { id: 17, src: '../../assets/es506_images/lvl3/pera.png' },
+  ]
+
+  ngOnInit() {
+    this.creaSequenza();
+    this.immagineCorrente = this.sequenzaImmagini[0];
+    this.mostraSequenza();
+  }
+
+  creaSequenza() {
+    switch (this.livello) {
+      case 1:
+        this.sequenzaImmagini = this.elementiLivello1;
+        break;
+      case 2:
+        this.sequenzaImmagini = this.elementiLivello2;
+        break;
+      case 3:
+        this.sequenzaImmagini = this.elementiLivello3;
+        break;
+    }
+    this.sequenzaImmagini = this.sequenzaImmagini.sort(() => 0.5 - Math.random());
+  }
+
+  mostraSequenza() {
+    let contatore = 1;
+    this.timer = setInterval(() => {
+      if (contatore < this.sequenzaImmagini.length) {
+        this.immagineCorrente = this.sequenzaImmagini[contatore];
+        contatore++;
+        console.log(this.immagineCorrente);
+      } else {
+        clearInterval(this.timer);
+        this.nextStep();
+      }
+    }, 500);
+  }
+
+  nextStep() {
+    this.step += 1;
+  }
+
+  seleziona(id: number) {
+    this.sequenzaSelezionata.push(id);
+    console.log("Hai selezionato" + this.sequenzaSelezionata);
+
+    if (this.sequenzaSelezionata.length == 6) {
+      this.verificaErrori();
+      this.nextStep();
+    }
+  }
+
+  verificaErrori() {
+    this.errori = 0;
+
+    for (let i = 0; i < 6; i++) {
+      if (this.sequenzaSelezionata[i] != this.sequenzaImmagini[i].id) {
+        this.errori += 1;
+      }
+    }
+  }
+}
--- src/app/esercizi/esercizi.routes.ts
+++ src/app/esercizi/esercizi.routes.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Es101Component } from './es101/es101.component';
-import { Es102Component } from './es102/es102.component';
 import { Es103Component } from './es103/es103.component';
 import { Es104Component } from './es104/es104.component';
 import { Es105Component } from './es105/es105.component';
@@ -9,12 +7,9 @@ import { Es108Component } from './es108/es108.component';
 import { Es109Component } from './es109/es109.component';
 import { Es110Component } from './es110/es110.component';
 import { Es303Component } from './es303/es303.component';
+import { Es506Component } from './es506/es506.component';
 const routes: Routes = [
   { path: '', redirectTo: '/esercizi', pathMatch: 'full' },
-  {
-    path: '101',
-    component: Es101Component,
-  },
   {
     path: '110',
     component: Es110Component,
@@ -47,6 +42,10 @@ const routes: Routes = [
     path: '303',
     component: Es303Component,
   },
+  {
+    path: '506',
+    component: Es506Component,
+  }
 ];
 
 @NgModule({
