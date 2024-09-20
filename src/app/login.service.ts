cfad712a9b79e41a74008c0d9c3a683a1e3a3018
Ettore Pane <ettore.pane@skywarder.eu>
2024-09-20 17:05:02 +0200

    Added AdminDashboard

---
+++ src/app/login.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(
+    private api: ApiService,
+    private router: Router
+  ) { }
+
+  login(username: string, password: string) {
+    this.api.post('login', { username, password }).subscribe((res: any) => {
+      console.log(res);
+
+      if (res.token !== undefined) {
+        localStorage.setItem('token', res.token);
+        //decode token
+        const token = res.token.split('.')[1];
+        const decodedToken = JSON.parse(atob(token));
+        localStorage.setItem('userInfos', JSON.stringify(decodedToken));
+        alert('Login effettuato con successo');
+        this.router.navigate(['/dashboard']);
+      } else {
+        alert('Errore durante il login');
+      }
+    });
+  }
+}
