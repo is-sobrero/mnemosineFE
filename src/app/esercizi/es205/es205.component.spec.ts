4a84866f695d40341293653bc1116b2535f8f748
AlexGuasco <alexguasco2008@gmail.com>
2024-09-20 15:03:43 +0200

    [IN PROGRESS] Esercizio 205

--- src/app/esercizi/es205/es205.component.spec.ts
+++ src/app/esercizi/es205/es205.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Es407Component } from './es407.component';
+import { Es205Component } from './es205.component';
 
-describe('Es407Component', () => {
-  let component: Es407Component;
-  let fixture: ComponentFixture<Es407Component>;
+describe('Es205Component', () => {
+  let component: Es205Component;
+  let fixture: ComponentFixture<Es205Component>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Es407Component]
+      imports: [Es205Component]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(Es407Component);
+    fixture = TestBed.createComponent(Es205Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/esercizi/es205/es205.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-es205',
+  standalone: true,
+  imports: [],
+  templateUrl: './es205.component.html',
+  styleUrl: './es205.component.scss'
+})
+export class Es205Component {
+
+}
