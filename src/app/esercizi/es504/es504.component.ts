532dc1dbbf467fe458ad6c3b13a711cf5c53390e
fil-cuk <felepocuk95@gmail.com>
2024-10-30 15:50:40 +0100

    ES504_INPROGRESS

---
+++ src/app/esercizi/es504/es504.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-es504',
+  standalone: true,
+  imports: [],
+  templateUrl: './es504.component.html',
+  styleUrl: './es504.component.scss'
+})
+export class Es504Component {
+
+}
