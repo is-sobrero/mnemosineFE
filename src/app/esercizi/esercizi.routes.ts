e01bc297a20137afc1345bcd0d29efbba5f36a38
GiacoLup <giacomoluppino8@gmail.com>
2024-11-20 15:08:22 +0100

    [FIXED] Esercizio 106

--- src/app/esercizi/esercizi.routes.ts
+++ src/app/esercizi/esercizi.routes.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Es106Component } from './es106/es106.component';
+import { Es101Component } from './es101/es101.component';
+import { Es102Component } from './es102/es102.component';
 
 
 const routes: Routes = [
@@ -8,6 +10,14 @@ const routes: Routes = [
   {
     path: '106',
     component: Es106Component,
+  },
+  {
+    path: '101',
+    component: Es101Component,
+  },
+  {
+    path: '102',
+    component: Es102Component,
   }
 ];
 
