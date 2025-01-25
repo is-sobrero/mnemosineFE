34958825a79a5ef633db51fc88ce909d968fabe7
GiacoLup <giacomoluppino8@gmail.com>
2025-01-25 16:51:38 +0100

    [COMPLETATO ED IMPLEMENTATO] Es. 504 Aggiornate immagini dei puzzle

--- src/app/esercizi/es106/es106.component.ts
+++ src/app/esercizi/es106/es106.component.ts
@@ -13,6 +13,7 @@ import {
   MatCardContent,
 } from '@angular/material/card';
 import { __values } from 'tslib';
+import { time } from 'console';
 
 @Component({
   selector: 'app-es106',
@@ -50,6 +51,11 @@ export class Es106Component implements OnInit {
   seed = 'abracadabra';
   constructor(private EX: ExerciseService) { }
   ngOnInit(): void {
+    setInterval(() => {
+      this.timeMillis += 100;
+    }
+    , 100);
+
     var dif = this.EX.currentInfo().difficulty;
     if (dif == 1) {
       this.setLivello(4);
