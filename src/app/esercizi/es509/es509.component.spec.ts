fa07c87a6c5a8c97e10f90dfc89eaa22a31b1583
fil-cuk <felepocuk95@gmail.com>
2025-01-27 22:24:30 +0100

    ES309 FIXATO

--- src/app/esercizi/es509/es509.component.spec.ts
+++ src/app/esercizi/es509/es509.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+<<<<<<<< HEAD:src/app/esercizi/es509/es509.component.spec.ts
 import { Es509Component } from './es509.component';
 
 describe('Es509Component', () => {
@@ -13,6 +14,21 @@ describe('Es509Component', () => {
     .compileComponents();
     
     fixture = TestBed.createComponent(Es509Component);
+========
+import { Es309Component } from './es309.component';
+
+describe('Es309Component', () => {
+  let component: Es309Component;
+  let fixture: ComponentFixture<Es309Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Es309Component]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(Es309Component);
+>>>>>>>> 424ddab (ES309 FIXATO):src/app/esercizi/es309/es309.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
