b3bc3d57852815ce1fca98848ba83feb3950fda8
AlexGuasco <alexguasco2008@gmail.com>
2025-01-29 16:20:35 +0100

    porca puttana

---
+++ src/app/esercizi/es208/es208.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-es208',
+  standalone: true,
+  imports: [],
+  templateUrl: './es208.component.html',
+  styleUrl: './es208.component.scss'
+})
+export class Es208Component {
+
+}
