d48f5c2d26b6b81d6f1e6db463c1dc2f5ef95e47
GiacoLup <giacomoluppino8@gmail.com>
2025-02-26 21:23:58 +0100

    [COMPLETATO] Esercizio 409
    Co-authored-by: AlexGuasco <AlexGuasco@users.noreply.github.com>

--- src/app/esercizi/es409/es409.component.ts
+++ src/app/esercizi/es409/es409.component.ts
@@ -47,13 +47,16 @@ export class Es409Component implements OnInit {
   step = 1;
   level = 0;
   errori = 0;
-  timeMillis: any;
+  timeMillis: any = 0;
   listCampi:any[] = []; //Contiene la lista dei campi semantici per livello
   listParole:any[] = []; //Contiene la lista della parole per livello
   nParole = 0; //Numero di parole mostrate
   nAssociazioni = 0; //Contatore per il numero di parole da associare
   random : number = 0; //Indice randomico per estrarre il campo
   campo: any; //Contiene il campo estratto randomicamente tra i tre del livello
+  //In aiuto all'utente
+  temp = 0; //Variabile temporanea per salvare il valore di nAccensioni prima che scali
+  counter = 0; //Segna all'utente il numero di parole da selezionare
 
   ngOnInit(): void {
     this.level = this.ES.currentInfo().difficulty;
@@ -63,47 +66,43 @@ export class Es409Component implements OnInit {
         this.nParole = 3;
         this.listCampi = ["scuola", "natura", "musica"];
         this.listParole = [
-          {word: "matita", field: "scuola"},
-          {word: "quaderno", field: "scuola"},
-          {word: "lavagna", field: "scuola"},
-          {word: "banco", field: "scuola"},
-          {word: "lezione", field: "scuola"},
-
-          {word: "albero", field: "natura"},
-          {word: "fiore", field: "natura"},
-          {word: "giume", field: "natura"},
-          {word: "montagna", field: "natura"},
-          {word: "foglia", field: "natura"},
-
-          {word: "nota", field: "musica"},
-          {word: "strumento", field: "musica"},
-          {word: "melodia", field: "musica"},
+          {word: "matita", field: "scuola", active: true},
+          {word: "quaderno", field: "scuola", active: true},
+          {word: "lavagna", field: "scuola", active: true},
+          {word: "banco", field: "scuola", active: true},
+          {word: "lezione", field: "scuola", active: true},
+          {word: "albero", field: "natura", active: true},
+          {word: "fiore", field: "natura", active: true},
+          {word: "fiume", field: "natura", active: true},
+          {word: "montagna", field: "natura", active: true},
+          {word: "foglia", field: "natura", active: true},
+          {word: "nota", field: "musica", active: true},
+          {word: "strumento", field: "musica", active: true},
+          {word: "melodia", field: "musica", active: true},
           {word: "ritmo", field: "musica"},
-          {word: "canzone", field: "musica"},
+          {word: "canzone", field: "musica", active: true},
         ];
       break;
 
       case 2:
         this.nParole = 3;
-        this.listCampi = ["mare", "tempo", "cibo"];
+        this.listCampi = ["mare", "astronomia", "cibo"];
         this.listParole = [
-          {word: "litorale", field: "mare"},
-          {word: "marea", field: "mare"},
-          {word: "scogliera", field: "mare"},
-          {word: "onda", field: "mare"},
-          {word: "battigia", field: "mare"},
-
-          {word: "brezza", field: "tempo"},
-          {word: "temporale", field: "tempo"},
-          {word: "umidità", field: "tempo"},
-          {word: "foschia", field: "tempo"},
-          {word: "grandine", field: "tempo"},
-
-          {word: "sapore", field: "cibo"},
-          {word: "aromi", field: "cibo"},
-          {word: "condimento", field: "cibo"},
-          {word: "pasto", field: "cibo"},
-          {word: "digestione", field: "cibo"},
+          {word: "litorale", field: "mare", active: true},
+          {word: "marea", field: "mare", active: true},
+          {word: "scogliera", field: "mare", active: true},
+          {word: "onda", field: "mare", active: true},
+          {word: "battigia", field: "mare", active: true},
+          {word: "costellazione", field: "astronomia", active: true},
+          {word: "nebulosa", field: "astronomia", active: true},
+          {word: "orbita", field: "astronomia", active: true},
+          {word: "supernova", field: "astronomia", active: true},
+          {word: "eclissi", field: "astronomia", active: true},
+          {word: "sapore", field: "cibo", active: true},
+          {word: "aromi", field: "cibo", active: true},
+          {word: "condimento", field: "cibo", active: true},
+          {word: "pasto", field: "cibo", active: true},
+          {word: "digestione", field: "cibo", active: true},
         ];
       break;
 
@@ -111,23 +110,21 @@ export class Es409Component implements OnInit {
         this.nParole = 5;
         this.listCampi = ["medicina", "tecnologia", "geografia"];
         this.listParole = [
-          {word: "febbre", field: "medicina"},
-          {word: "sintomo", field: "medicina"},
-          {word: "cura", field: "medicina"},
-          {word: "infezione", field: "medicina"},
-          {word: "guarigione", field: "medicina"},
-
-          {word: "rete", field: "tecnologia"},
-          {word: "software", field: "tecnologia"},
-          {word: "algoritmo", field: "tecnologia"},
-          {word: "connessione", field: "tecnologia"},
-          {word: "dispositivo", field: "tecnologia"},
-
-          {word: "estuario", field: "geografia"},
-          {word: "altopiano", field: "geografia"},
-          {word: "canyon", field: "geografiao"},
-          {word: "montagna", field: "geografia"},
-          {word: "tundra", field: "geografia"},
+          {word: "febbre", field: "medicina", active: true},
+          {word: "sintomo", field: "medicina", active: true},
+          {word: "cura", field: "medicina", active: true},
+          {word: "infezione", field: "medicina", active: true},
+          {word: "guarigione", field: "medicina", active: true},
+          {word: "rete", field: "tecnologia", active: true},
+          {word: "software", field: "tecnologia", active: true},
+          {word: "algoritmo", field: "tecnologia", active: true},
+          {word: "connessione", field: "tecnologia", active: true},
+          {word: "dispositivo", field: "tecnologia", active: true},
+          {word: "estuario", field: "geografia", active: true},
+          {word: "altopiano", field: "geografia", active: true},
+          {word: "canyon", field: "geografia", active: true},
+          {word: "montagna", field: "geografia", active: true},
+          {word: "tundra", field: "geografia", active: true},
         ];
       break;
     }
@@ -151,12 +148,19 @@ export class Es409Component implements OnInit {
 
       for(let i = 0; i < this.nParole; i++){
         if(this.campo == this.listParole[i].field){
-          this.listParole = this.listParole.slice(0, this.nParole);
           found = true;
           break;
         }
       }
     }
+
+    this.listParole = this.listParole.slice(0, this.nParole);
+
+    for(let i = 0; i < this.nParole; i++)
+      if(this.listParole[i].field == this.campo)
+          this.nAssociazioni++;
+
+    this.temp = this.nAssociazioni;
   }
 
   shuffleArray(array: any[]): any[] {
@@ -165,5 +169,16 @@ export class Es409Component implements OnInit {
       [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
+  }
+
+  checkErrors(field:any){
+    if(field != this.campo)
+        this.errori++;
+    
+    this.nAssociazioni--;
+    this.counter++;
+
+    if(this.nAssociazioni == 0)
+      this.ES.nextExercise(409, {errors: this.errori, time: this.timeMillis});
   }
 }
