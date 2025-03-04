b548696dc5b226f9241506b73886c26154ce691e
Alex2300 <misuralealessandro@gmail.com>
2025-03-04 13:32:07 +0100

    esdercizio 206 implementato

--- src/app/esercizi/es206/es206.component.ts
+++ src/app/esercizi/es206/es206.component.ts
@@ -9,6 +9,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { __await } from 'tslib';
 import { start } from 'repl';
 import { ExerciseService } from '../../exercise.service';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-es206',
@@ -32,7 +33,7 @@ import { ExerciseService } from '../../exercise.service';
   templateUrl: './es206.component.html',
   styleUrl: './es206.component.scss'
 })
-export class Es206Component {
+export class Es206Component implements AfterViewInit, OnInit{
   constructor(private ES: ExerciseService) { }
   current_state = 0;
   errors:number = 0;
@@ -48,47 +49,86 @@ export class Es206Component {
       this.timeMillis += 500;
     }, 500);
 
-    //this.level = this.ES.currentInfo().difficulty;
-    this.level = 1;
+    this.level = this.ES.currentInfo().difficulty;
+    //this.level = 1;
 
     switch(this.level){
       case 1:
         this.speed_index = 1;
         break;
     case 2:
-        this.speed_index = 2;
+        this.speed_index = 1.4;
         break;
     case 3:
-        this.speed_index = 3;
+        this.speed_index = 2;
+        break;
+    default:
+        this.speed_index = 1;
         break;
     }
+  }
 
+  ngAfterViewInit(){
+    var can:HTMLCanvasElement = document.getElementById("game-ctx")! as HTMLCanvasElement;
+    var ctx:CanvasRenderingContext2D = can.getContext("2d")! as CanvasRenderingContext2D;
+    let WIDTH:number = 400;
+    let HEIGHT:number = 200;
+    ctx.canvas.width = WIDTH;
+    ctx.canvas.height = HEIGHT;
+    let BAR_WIDTH:number = 20;
+    let BAR_HEIGHT:number = 100;
+    let DELTA_TIME = 16; // in milliseconds: 16ms = 60 frame per second
+    var random_pos_x:number = BAR_WIDTH+Math.floor(Math.random()*(WIDTH-(BAR_WIDTH*2)));
+    let c_pos_x:number = 0;
+    let BACKGROUND = "rgba(255,255,255,255)";
+    let RC_COLOR = "rgba(74,74,74,255)";
+    let C_COLOR = "rgba(232, 87, 14,255)";
+
+    let end:boolean = false;
+    const gameLoop = ()=>{
+      let direction:number = 2;
+      let int = setInterval(() => {
+          if(c_pos_x > WIDTH-BAR_WIDTH/2) direction = direction*-1;
+          if(c_pos_x < 0) direction = direction*-1;
+
+          ctx.beginPath();
+
+          ctx.fillStyle = BACKGROUND;
+          ctx.fillRect(0,0, WIDTH, HEIGHT);
+          ctx.fillStyle = RC_COLOR;
+          ctx.fillRect(random_pos_x,BAR_HEIGHT-HEIGHT/4,BAR_WIDTH,BAR_HEIGHT);
+          ctx.fillStyle = C_COLOR;
+          ctx.fillRect(c_pos_x, BAR_HEIGHT-HEIGHT/4, BAR_WIDTH/2, BAR_HEIGHT);
+          c_pos_x += direction*this.speed_index;
+
+          ctx.stroke();
+          if(end) clearInterval(int);
+
+      }, DELTA_TIME);
+    };
+    gameLoop();
     document.addEventListener("keydown", (ev:KeyboardEvent)=>{
       if(ev.key == " "){
         this.input.push(ev.key);
       }
     });
     document.addEventListener("keyup", (ev:KeyboardEvent)=>{
-      if(!(typeof (this.input.find((el)=> el == ev.key)) == undefined)){
-        this.catch_line();
+      if(this.input.indexOf(" ") != -1){
+        if(
+            Math.floor(c_pos_x) >= random_pos_x && Math.floor(c_pos_x) <= random_pos_x+BAR_WIDTH
+        ){
+          console.log("Cursor Inside the grey bar");
+          this.current_state = 1;
+          end = true;
+          setTimeout(() => {
+            this.ES.nextExercise(404, { errors: this.errors , time: this.timeMillis });
+          }, 1000);
+        }else{
+          this.errors += 1;
+        }
+        if(this.errors > 6) this.allow_skip = true;
       }
     });
-  }
-
-  catch_line(){
-    console.log("catching line");
-
-    /*
-     *  here the main logic that will check if the moving bar would be on top
-     * of the red one will take place
-     *
-     *
-     * */
-
-    if(this.errors > 6) this.allow_skip = true;
-
-
-
   }
 
   skip_exercise(){
